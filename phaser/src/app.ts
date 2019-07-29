c464a6fa75b5b66031fba4a27b66aea35a99f998
Law Smith <lawsumisu@gmail.com>
2019-07-28 19:26:12 -0700

    Project/phaser template (#1)
    * added phaser template + TS.
    
    * phazer -> phaser
    
    * Finished tslint setup.

---
+++ phaser/src/app.ts
@@ -0,0 +1,32 @@
+import * as Phaser from 'phaser';
+import logoImg from 'src/assets/logo.png';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: {
+    preload,
+    create,
+  },
+};
+
+new Phaser.Game(config);
+
+function preload(this: Phaser.Scene) {
+  this.load.image('logo', logoImg);
+}
+
+function create(this: Phaser.Scene) {
+  const logo = this.add.image(400, 150, 'logo');
+
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: 'Power2',
+    yoyo: true,
+    loop: -1,
+  });
+}
